e4e2b4927a8c0763715400514becbb4eaecd2db9
Chris Fulstow <chris.fulstow@gmail.com>
2010-11-19 04:27:17 +0000

    Edit entry form styling

--- NBlog.Web/Controllers/EntryController.Models.cs
+++ NBlog.Web/Controllers/EntryController.Models.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,8 @@ namespace NBlog.Web.Controllers
         public class EditModel : LayoutModel
         {
             public string Slug { get; set; }
+
+            [DisplayName("Slug")]
             public string NewSlug { get; set; }
             
             [Required]
